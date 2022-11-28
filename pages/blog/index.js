edce908000f33d706f384dd066c95405d884533d
harsh1x4 <harshbsi567@gmail.com>
2022-11-28 16:19:35 +0530

    Trial with Remote Image

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -13,21 +13,21 @@ const blog = () => {
         </h1>
         <div style={{ "text-align": "center", "display": "flex", "justify-content": "center" }}>
           <div className={styles.grid}>
-            {/* <a className={styles.card}>
-            <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog3"></Image>
-          </a>
-          <a className={styles.card}>
-            <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog4"></Image>
-          </a>
-          <a className={styles.card}>
-            <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog3"></Image>
-          </a>
-          <a className={styles.card}>
-            <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog4"></Image>
-          </a>
-          <a className={styles.card}>
-            <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog3"></Image>
-          </a> */}
+            <a className={styles.card}>
+              <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog3"></Image>
+            </a>
+            <a className={styles.card}>
+              <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog4"></Image>
+            </a>
+            <a className={styles.card}>
+              <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog3"></Image>
+            </a>
+            <a className={styles.card}>
+              <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog4"></Image>
+            </a>
+            <a className={styles.card}>
+              <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog3"></Image>
+            </a>
           </div>
         </div>
       </div>
