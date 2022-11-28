0f6947890c74bbd0a5127ed2a6e27a579a3175c1
harsh1x4 <harshbsi567@gmail.com>
2022-11-28 16:07:36 +0530

    Commit for deployment

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,19 @@
-import styles from '../../styles/Home.module.css'
+// import styles from '../../styles/Home.module.css'
 import Image from 'next/image';
+import styles from '../../styles/blogIndex.module.css'
+import Navbar from '../components/Navbar.js'
 
 const blog = () => {
   return (
     <>
-      <h1 className={styles.title}>
-        Welcome to <a href="#">Blog</a>
-      </h1>
-      <div style={{ "text-align": "center", "display": "flex", "justify-content": "center" }}>
-        <div className={styles.grid}>
-          {/* <a className={styles.card}>
+      <Navbar />
+      <div className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="#">Blog</a>
+        </h1>
+        <div style={{ "text-align": "center", "display": "flex", "justify-content": "center" }}>
+          <div className={styles.grid}>
+            {/* <a className={styles.card}>
             <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog3"></Image>
           </a>
           <a className={styles.card}>
@@ -24,6 +28,7 @@ const blog = () => {
           <a className={styles.card}>
             <Image src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={300} width={500} alt="blog3"></Image>
           </a> */}
+          </div>
         </div>
       </div>
     </>
