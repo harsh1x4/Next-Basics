5f36854a31f3027adb3adf992dbef049c4669b49
harsh1x4 <harshbsi567@gmail.com>
2022-12-03 23:47:39 +0530

    trial error removal

--- pages/commentsCard.js
+++ pages/commentsCard.js
@@ -12,7 +12,12 @@ const commentsCard = () => {
       </Head>
       <div className='grid grid-cols-1 gap-1 md:grid-cols-3 lg:grid-cols-4'>
         {data.comments.map((comment) => (
-          <CommentTemplate comment={comment} key={comment.id} />
+          // <CommentTemplate comment={comment} key={comment.id} />
+          <div className='card' key={comment.id}>
+            <h2 className='text-xl font-bold'>{comment.name}</h2>
+            <h6 className='font-semibold'>Email: {comment.email}</h6>
+            <p className="text-slate-300 font-light">{comment.body}</p>
+          </div>
         ))}
       </div>
     </>
