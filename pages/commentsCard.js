b55a6338d41d50910d350f30970fcc1d4f4280f4
harsh1x4 <harshbsi567@gmail.com>
2022-12-03 23:03:21 +0530

    error removal trial

--- pages/commentsCard.js
+++ pages/commentsCard.js
@@ -5,7 +5,7 @@ const commentsCard = () => {
   return (
     <div className='grid grid-cols-1 gap-1 md:grid-cols-3 lg:grid-cols-4'>
     {data.comments.map((comment) => (
-        <CommentsTemplate comments={comment} key={comment.id}/>
+        <CommentsTemplate comment={comment} key={comment.id}/>
     ))}
     </div>
   )
--- pages/components/CommentsTemplate.js
+++ pages/components/CommentsTemplate.js
@@ -1,10 +1,10 @@
 
-const CommentsTemplate = ({comments}) => {
+const CommentsTemplate = ({comment}) => {
   return (
     <div className='card'>
-        <h2 className='text-xl font-bold'>{comments.name}</h2>
-        <h6 className='font-semibold'>Email: {comments.email}</h6>
-        <p className="text-slate-300 font-light">{comments.body}</p>
+        <h2 className='text-xl font-bold'>{comment.name}</h2>
+        <h6 className='font-semibold'>Email: {comment.email}</h6>
+        <p className="text-slate-300 font-light">{comment.body}</p>
     </div>
   )
 }
