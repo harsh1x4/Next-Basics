1a8c635e3f32ce23204057d2f8cf4bdb1a0fb1e9
harsh1x4 <harshbsi567@gmail.com>
2022-12-04 10:42:49 +0530

    Template added

---
+++ CommentTemplate.js
@@ -0,0 +1,12 @@
+
+const CommentTemplate = ({ comment }) => {
+  return (
+    <div className='card'>
+      <h2 className='text-xl font-bold'>{comment.name}</h2>
+      <h6 className='font-semibold'>Email: {comment.email}</h6>
+      <p className="text-slate-300 font-light">{comment.body}</p>
+    </div>
+  )
+}
+
+export default CommentTemplate;
--- pages/commentsCard.js
+++ pages/commentsCard.js
@@ -1,5 +1,6 @@
 import data from '../utils/data';
 import Head from 'next/head';
+import CommentTemplate from '../CommentTemplate';
 // import CommentTemplate from './components/CommentTemplate';
 
 const commentsCard = () => {
@@ -12,12 +13,12 @@ const commentsCard = () => {
       </Head>
       <div className='grid grid-cols-1 gap-1 md:grid-cols-3 lg:grid-cols-4'>
         {data.comments.map((comment) => (
-          // <CommentTemplate comment={comment} key={comment.id} />
-          <div className='card' key={comment.id}>
-            <h2 className='text-xl font-bold'>{comment.name}</h2>
-            <h6 className='font-semibold'>Email: {comment.email}</h6>
-            <p className="text-slate-300 font-light">{comment.body}</p>
-          </div>
+          <CommentTemplate comment={comment} key={comment.id} />
+          // <div className='card' key={comment.id}>
+          //   <h2 className='text-xl font-bold'>{comment.name}</h2>
+          //   <h6 className='font-semibold'>Email: {comment.email}</h6>
+          //   <p className="text-slate-300 font-light">{comment.body}</p>
+          // </div>
         ))}
       </div>
     </>
